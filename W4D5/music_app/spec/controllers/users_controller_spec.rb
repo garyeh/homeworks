e3db100539b0c9bd7f7a91604b4b658753535d57
Gary Yeh <garyeh@gmail.com>
2017-04-21 08:19:01 -0700

    Add integration tests to HW

--- W4D5/music_app/spec/controllers/users_controller_spec.rb
+++ W4D5/music_app/spec/controllers/users_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe UsersController, :type => :controller do
 
@@ -11,13 +12,25 @@ RSpec.describe UsersController, :type => :controller do
 
   describe "POST #create" do
     context "with invalid params" do
-      it "validates the presence of the user's email and password"
+      it "validates the presence of the user's email and password" do
+        post :create, user: {email: "gary@gary.com", password: ""}
+        expect(response).to render_template("new")
+        expect(flash[:errors]).to be_present
+      end
 
-      it "validates that the password is at least 6 characters long"
+      it "validates that the password is at least 6 characters long" do
+        post :create, user: {email: "gary@gary.com", password: "pw"}
+        expect(response).to render_template("new")
+        expect(flash[:errors]).to be_present
+      end
     end
 
     context "with valid params" do
-      it "redirects user to bands index on success"
+      it "redirects user to bands index on success" do
+        post :create, user: {email: "gary@gary.com", password: "password"}
+        expect(flash[:errors]).not_to be_present
+        expect(response).to redirect_to(new_session_url)
+      end
     end
   end
 end
