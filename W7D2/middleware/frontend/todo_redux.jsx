7c9dabe23dfacc0c9a945ed6575a0481a57d0b13
Gary Yeh <garyeh@gmail.com>
2017-05-09 08:34:12 -0700

    Complete middleware HW

--- W7D2/middleware/frontend/todo_redux.jsx
+++ W7D2/middleware/frontend/todo_redux.jsx
@@ -4,6 +4,7 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
+
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
@@ -12,3 +13,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+// const addLoggingToDispatch = store => next => action => {
+//   const OGDispatch = store.dispatch;
+//   console.log(store.getState());
+//   console.log(action);
+//   let returnValue = OGDispatch(action);
+//   console.log(store.getState());
+//   return returnValue;
+// };
+//
+// const applyMiddlewares = (store, ...middlewares) => {
+//   let dispatch = store.dispatch;
+//   middlewares.forEach((middleware) => {
+//     dispatch = middleware(store)(dispatch);
+//   });
+//   return Object.assign({}, store, { dispatch });
+// };
