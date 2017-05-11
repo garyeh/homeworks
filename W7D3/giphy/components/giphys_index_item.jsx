f683d3ebecc44aa0e4f0600484dea8764c9d3eee
Gary Yeh <garyeh@gmail.com>
2017-05-11 08:25:20 -0700

    Add Rainbow Router HW

--- W7D3/giphy/components/giphys_index_item.jsx
+++ W7D3/giphy/components/giphys_index_item.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const GiphysIndexItem = ({ giphy }) => (
-    <li className="giphy-li">
-      <img src={giphy.images.fixed_height.url} />
-    </li>
+  <li className="giphy-li">
+    <img src={giphy.images.fixed_height.url} />
+  </li>
 );
 
 export default GiphysIndexItem;
